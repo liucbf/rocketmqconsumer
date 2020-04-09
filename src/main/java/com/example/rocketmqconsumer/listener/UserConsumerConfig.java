5197bff66235988a26309fbae284ca957d22353b
liucbf <839513244@qq.com>
2020-04-09 19:39:11 +0800

    first commit

---
+++ src/main/java/com/example/rocketmqconsumer/listener/UserConsumerConfig.java
@@ -0,0 +1,42 @@
+package com.example.rocketmqconsumer.listener;
+
+import com.alibaba.fastjson.JSON;
+import com.example.demo.mqconfig.DefaultConsumerConfig;
+import com.example.demo.mqconfigdemo.UserMessageListenerConcurrently;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UserConsumerConfig {
+
+    @Value("${rocketmq.consumer.namesrvAddr}")
+    private String namesrvAddr;
+
+    @Value("${rocketmq.consumer.groupName}")
+    private String groupName;
+
+    @Value("${rocketmq.consumer.topic}")
+    private String topic;
+
+    @Value("${rocketmq.consumer.tag}")
+    private String tag;
+
+    @Value("${rocketmq.consumer.consumeThreadMin}")
+    private int consumeThreadMin;
+
+    @Value("${rocketmq.consumer.consumeThreadMax}")
+    private int consumeThreadMax;
+
+
+
+    @Bean
+    public DefaultConsumerConfig DefaultConsumer() {
+        DefaultConsumerConfig consumer = new DefaultConsumerConfig(namesrvAddr, groupName, topic, tag,new UserMessageListenerConcurrently() );
+        System.out.println("================>user消费者创建完成<================"+ JSON.toJSONString(consumer));
+        return consumer;
+    }
+
+
+
+}
